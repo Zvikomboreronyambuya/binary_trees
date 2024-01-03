24039c37e3a225eae8513b6fd47f158982db1f94
Zvikomboreronyambuya <znyambuya@gmail.com>
2024-01-03 01:43:31 -0800

    added test

--- binary_trees.h
+++ binary_trees.h
@@ -90,4 +90,4 @@ int is_perfect_recursive(const binary_tree_t *tree,
 		size_t leaf_depth, size_t level);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H *
+#endif /* BINARY_TREES_H */
